6e23110b5c5c4c051451ca495c3de609874ab3fa
weihz <315@qq.com>
2020-03-11 15:12:24 +0800

    增加日志

--- src/main/java/com/css/webservice/controller/ToDoApiController.java
+++ src/main/java/com/css/webservice/controller/ToDoApiController.java
@@ -4,12 +4,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
+import com.css.addbase.msg.MsgTipUtil;
 import com.css.app.qxjgl.business.entity.Leaveorback;
 import com.css.app.qxjgl.business.service.LeaveorbackService;
 import com.css.app.qxjgl.util.QxjStatusDefined;
@@ -21,12 +24,14 @@ public class ToDoApiController {
 	
 	@Autowired
     private LeaveorbackService leaveorbackService;
+	private static Logger logger = LoggerFactory.getLogger(ToDoApiController.class);
 	/**
      * 请销假待办总数
      */
     @ResponseBody
     @RequestMapping("/qxjgl/todo")
     public void qxjdbCount(){
+    	logger.info("桌面接口开始调用代办总数接口：","/api/qxjgl/todo");
         JSONObject jsonObject = new JSONObject();
         String userId = CurrentUser.getUserId();
         Map<String, Object> map = new HashMap<>();
@@ -46,6 +51,8 @@ public class ToDoApiController {
         int qxjspNum = this.distictTLeaveorback(leaveLists1);
         //请销假总的代办数
         jsonObject.put("qxjdbCount",qxjsqNum+qxjspNum);
+        jsonObject.put("result", "success");
+        logger.info("桌面接口成功结束调用代办总数接口：","/api/qxjgl/todo");
         Response.json(jsonObject);
     }
     
