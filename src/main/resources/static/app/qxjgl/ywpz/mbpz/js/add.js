af8d750f8bde00b0cb937cd527e3a58270dd2dc2
wangxuan <Administrator@User-2020XDPAKV>
2020-06-23 15:55:49 +0800

    请销假代码修改

--- src/main/resources/static/app/qxjgl/ywpz/mbpz/js/add.js
+++ src/main/resources/static/app/qxjgl/ywpz/mbpz/js/add.js
@@ -1,53 +1,19 @@
 var saveUrl = {"url":"/app/qxjgl/qxjdicusers/save","dataType":"text"};
-var userTree = {"url":"/app/base/user/tree","dataType":"text"}; //人员选择树
+var userTree = {"url":"/app/base/dept/tree_onlyroot","dataType":"text"}; //人员选择树
 
 var pageModule = function(){
 	var initother = function(){
-		
-		$("#username").createUserTree({
+		//部门选择
+		$("#deptName").createUserTree({
 			url : userTree,
-			width : "100%",
+			plugins:"checkbox",
+			width:"100%",
 			success : function(data, treeobj) {},
-			selectnode : function(e, data) {
-				$("#username").val(data.node.text);
-				$("#userid").val(data.node.id);
-				var deptid = $("#usernametree2").jstree().get_parent(data.node.id);
-				$("#deptid").val(deptid);
+			selectnode : function(e, data,treessname,treessid) {
+				$("#deptName").val(treessname);
+				$("#deptId").val(treessid);
 			}
 		});
-		
-		$("#quxiao").click(function(){
-			newbootbox.newdialogClose("adddialog");
-		})
-		
-		$("#save").click(function(){
-			var username=$("#username").val();
-			var userid=$("#userid").val();
-			var deptid=$("#deptid").val();
-			if(userid == ''){
-				newbootbox.alertInfo("请选择用户！");
-				return;
-			}
-			$ajax({
-				url:saveUrl,
-				data:{username:username,userid:userid,deptid:deptid,rolecode:$("#rolecode").val(),rolename:$("#rolecode").find("option:selected").text()},
-				type: "GET",
-				success:function(data){
-					newbootbox.newdialogClose("adddialog");
-					if(data.msg == "success") {
-						newbootbox.alertInfo('保存成功！').done(function(){
-							window.top.iframe1.window.iframe2.window.pageModule.reload();
-						});
-					}else if(data.msg == "repeat") {
-						newbootbox.alertInfo('该用户已定义角色,如想定义新角色请先删除原有角色!').done(function(){
-						});
-					}else{
-						newbootbox.alertInfo("保存失败！");
-					}
-				}
-			});
-			
-		})
 	}
 	
 	return{
